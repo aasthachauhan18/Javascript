f95325a134350328c8db2c82d511e2c469590550
Aastha1810-stack <chauhanaastha1810@gmail.com>
2025-08-26 16:14:30 +0530

    26/8/2025

---
+++ Presen.py
@@ -0,0 +1,47 @@
+# Write a Python program to swap two variables without using a third variable.
+
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+
+# a,b = b,a
+
+# print(a)
+# print(b)
+
+
+# Write a program that accepts two integers and prints their sum, difference, product, and quotient.
+
+# num1 = int(input("Enter first number:"))
+# num2=int(input("Enter secind number:"))
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# Write a program that calculates the square and cube of a given number using exponent operator **.
+
+# num1= int(input("Enter number"))
+# print(num1**2)
+# print(num1**3)
+# Write a program to check if a given year is a leap year.
+# year = int(input("Enter year:"))
+
+# if year%400==0 or(year%4==0 and year %100 !=0):
+#     print("Leap year.")
+# else:
+#     print("Not a leap year.")
+
+# Print all even numbers from 1 to 50.
+
+# for i in range(1,51):
+#     if i%2==0:
+#         print(i)
+
+# Write a program to count the number of vowels in a given string.
+
+ch = input("Enter Character: ")
+
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+    print("Its Vowels.")
+else:
+    print("Its contants")    
